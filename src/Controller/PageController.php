8c49bc265c838e991839e1a083a79f0d28d3b843
Josip Arambasic <jaramb00@fesb.hr>
2026-05-16 20:44:29 +0200

    Add frontend authentication pages - register, login, home

---
+++ src/Controller/PageController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/register', name: 'page_register')]
+    public function register(): Response
+    {
+        return $this->render('auth/register.html.twig');
+    }
+
+    #[Route('/login', name: 'page_login')]
+    public function login(): Response
+    {
+        return $this->render('auth/login.html.twig');
+    }
+
+    #[Route('/dashboard', name: 'page_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('dashboard/index.html.twig');
+    }
+
+    #[Route('/request/{id}', name: 'page_request_detail')]
+    public function requestDetail(int $id): Response
+    {
+        return $this->render('dashboard/detail.html.twig', ['requestId' => $id]);
+    }
+}
