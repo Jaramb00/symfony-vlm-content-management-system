0e5a7f04ad663fb00000c91c0e58900e618dd1cc
Josip Arambasic <jaramb00@fesb.hr>
2026-05-19 11:49:46 +0200

    Add RequestStatus constants for type safety

---
+++ src/Constants/RequestStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+class RequestStatus
+{
+    public const PENDING = 'pending';
+    public const PROCESSING = 'processing';
+    public const DONE = 'done';
+    public const FAILED = 'failed';
+}
